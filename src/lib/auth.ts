ddd0341f2efede41e7d32fa14622a63127abb7ee
MD. Sadat Alam Protik <protik0939@gmail.com>
2026-02-05 15:08:20 +0600

    Added production URL to trusted origins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -17,7 +17,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  trustedOrigins: [process.env.APP_URL!],
+  trustedOrigins: [process.env.APP_URL!, process.env.PROD_APP_URL!],
   baseURL: process.env.BETTER_AUTH_URL,
   session: {
     cookieCache: {
